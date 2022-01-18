19933250c3694284d6bbd5850a59cd38fdafd643
SapirShira <shirahalili78@gmail.com>
2022-01-18 22:52:54 +0200

    1

--- names/names/Program.cs
+++ names/names/Program.cs
@@ -11,18 +11,39 @@ namespace names
 
     public class fix_list
     {
-        public List<Similar_name> lst_sn { get; set; }
+        public List<List<string>> lst_sn { get; set; }
         public Dictionary<string, int> map_name { get; set; }
+         public void print_lst()
+         {
+             foreach (List<string> d in lst_sn)
+             {
+                 foreach(string n in d)
+                 {
+                     Console.Write(n + ' ');
+                 }
+                 Console.WriteLine();
+             }
+         }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Similar_name s = new Similar_name();
-            s.name_1 = "try 1";
-            s.name_2 = "try 2";
-            Console.WriteLine(s.name_1 + ' ' + s.name_2);
+
+            fix_list f = new fix_list();
+            f.map_name = new Dictionary<string, int>() { { "Jacob", 15 }, { "Yaakov", 12 }, { "Tomer", 13 }, { "Tommer", 4 }, { "Sara", 19 } };
+            f.lst_sn = new List<List<string>>();
+            f.lst_sn.Add(new List<string> { "Jacob", "Yaakov" });
+            f.lst_sn.Add(new List<string> { "Yaakov", "Yaacov" });
+            f.lst_sn.Add(new List<string> { "Tomer", "Tommer" });
+            foreach (KeyValuePair<string, int> d in f.map_name)
+            {
+                Console.WriteLine(d.Key + ' ' + d.Value);
+
+            }
+            f.print_lst();
+
         }
     }
 }
